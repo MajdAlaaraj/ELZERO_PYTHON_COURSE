ba17bf8a2a620ea001ea22e2167e78823d68e23b
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-03 19:05:18 +0300

    python projects

---
+++ Assignments 001-010.py
@@ -0,0 +1,16 @@
+#Assignment 1
+#This is multiple comment 
+#My name Majd Alaaraj
+#I am Computer and Automatic Control Engineer
+#................
+#Assignment 2
+name="Majd_Alaaraj"
+age="28"
+country="syria"
+#Assignment 3
+print("\"name :"+name+"\"")
+print("\"age :"+age+"\"")
+print("\"country :"+country+"\"")
+#Assignment 4
+print("Hello, My Name Is "+name+" And Iam "+ age+" Years Old and I Live in "+ country+".")
+
