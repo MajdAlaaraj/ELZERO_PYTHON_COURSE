1842743ba78049233a2efae496a4b2a487c41112
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-03 20:17:50 +0300

    python projects

---
+++ Assignments 011-018.py
@@ -0,0 +1,35 @@
+#Assignments 1
+name="Majd_Alaaraj"
+age="28"
+country="Syria"
+print("\"Hello \'"+name+"\',How You Doing \\\"\"\"Your Age Is \""+age+"\"\"+ And Your Country Is:"+country)
+#Assignments 2
+print("\"Hello \'"+name+"\'\n,How You Doing \\\"\"\"Your Age Is \""+age+"\"\"+\n And Your Country Is:"+country)
+#Assignment 3 
+name="Elzero"
+print("Second Letter Is\""+ name[1]+"\"")
+print("Third Letter Is \"" +name[2]+"\"")
+print("Last Letter Is \""+name[-1]+"\"")
+#Assignment 4
+print (name[1:4])
+print(name[0:6:2])
+print(name[-2:-7:-2])
+#Assignment 5
+name ="#@#@Elzero#@#@"
+print(name.strip("#@"))
+#Assignment 6
+num1="9"
+num2="15"
+num3="130"
+num4="950"
+num5="1300"
+print(num1.zfill(4))
+print(num2.zfill(4))
+print(num3.zfill(4))
+print(num4.zfill(4))
+print(num5.zfill(4))
+#Assignment 7
+
+
+
+
