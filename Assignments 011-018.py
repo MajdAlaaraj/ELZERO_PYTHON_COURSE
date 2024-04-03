4ca586c384937619fc66977a1ea2f858f0d67663
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-03 20:37:53 +0300

    python projects

--- Assignments 011-018.py
+++ Assignments 011-018.py
@@ -29,6 +29,33 @@ print(num3.zfill(4))
 print(num4.zfill(4))
 print(num5.zfill(4))
 #Assignment 7
+name_one="Osama"
+name_two="Osame_Alzero"
+print(name_one.rjust(20,"@"))
+print(name_two.rjust(20,"@"))
+#Assignment 8
+name_one="OsAmA"
+name_two="osAMa"
+print(name_one.swapcase())
+print(name_two.swapcase())
+#Assignment 9
+msg="I Love Python And Although Love Elzero Web School"
+print(msg.count("Love"))
+#Assignment 10 
+name="Elzero"
+print(name.find("z"))
+#Assignment 11
+msg="I <3 Python And Although <3 Elzero Web School"
+print(msg.replace("<3","Love",1))
+#Assignment 12
+msg="I <3 Python And Although <3 Elzero Web School"
+print(msg.replace("<3","Love"))
+#Assignment 13
+name = "Osama"
+age = 38
+country = "Egypt"
+print(f"My Name Is {name}, And My Age Is {age}, And My Country Is {country}")
+
 
 
 
