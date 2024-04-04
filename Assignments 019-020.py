25e7839e7ba2217cce841fce294dbfff5e09ca9c
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-04 05:04:05 +0300

    python projects

---
+++ Assignments 019-020.py
@@ -0,0 +1,22 @@
+#Assignment 1
+print(type(1))
+print(type(1.0))
+print(type(1+1j))
+#Assignment 2
+complex=1+2j
+print(complex.imag)
+print(complex.real)
+#Assignment 3
+num=10
+print("{:.10f}".format(num))
+#Assignment 4
+num=159.650
+print(int(num))
+print(type(int(num)))
+#Assignment 5
+# 100 - 115 = -15
+# 50 * 30 = 1500
+# 21 % 4 = 1
+# 110 / 11 = 10
+# 97 // 20 = 4
+
