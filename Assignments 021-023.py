384339576fa68192cc850669f5f3e72e00a41d3f
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-04 05:59:11 +0300

    python projects

---
+++ Assignments 021-023.py
@@ -0,0 +1,49 @@
+#Assignment 1
+friends=["Osama","Ahmad","Sayed","Ali","Mahmoud"]
+print(friends[0])
+print(friends[-5])
+print(friends[-1])
+print(friends[4])
+#Assignment 2
+print(friends[::2])
+print(friends[1::2])
+#Assignment 3
+print(friends[1:4])
+print(friends[-1:-3:-1])
+#Assignment 4
+friends[-1]="Elzero"
+friends[-2]="Elzero"
+print(friends)
+#Assignment 5
+friends=["Osama","Ahmad","Sayed"]
+friends.insert(0,"Naser")
+print(friends)
+friends.append("Salem")
+print(friends)
+#Assignment 6
+friends=['Naser', 'Osama', 'Ahmad', 'Sayed', 'Salem']
+friends.pop(0)
+friends.pop(0)
+print(friends)
+friends.pop(-1)
+print(friends)
+#Assignment 7
+friends=["Ahmad","Sayed"]
+employees=["Sameh","Eman"]
+School=["Ramy","Shady"]
+friends.extend(employees)
+friends.extend(School)
+print(friends)
+#Assignment 8
+friends=['Ahmad', 'Sayed', 'Sameh', 'Eman', 'Ramy', 'Shady']
+friends.sort()
+print(friends)
+friends=['Ahmad', 'Sayed', 'Sameh', 'Eman', 'Ramy', 'Shady']
+friends.sort(reverse=True)
+print(friends)
+#Assignment 9
+friends=['Ahmad', 'Sayed', 'Sameh', 'Eman', 'Ramy', 'Shady']
+
+
+
+
