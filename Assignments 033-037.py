52ac7d07f208a44abace3ebc55fea354dd000496
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-06 01:33:41 +0300

    python projects

---
+++ Assignments 033-037.py
@@ -0,0 +1,39 @@
+#Assignment 1
+print(5>3)
+print(6==6.0)
+print(1==True)
+print("ahmad"!="mohamad")
+print(3>9)
+print(5==50)
+print("ahmad"=="mohamad")
+print(9>=25)
+#Assignment 2
+html=80
+css=60
+javascript=70
+print(html>50 and css>50 and javascript>50)
+#Assignment 3
+num_one=10
+num_two=20
+num=20
+print(num>num_one or num>num_two)
+print(num>num_one and num>num_two)
+#Assignment 4
+num_one=10
+num_two=20
+result=num_one+num_two
+print(result)
+print(result**3)
+print((result**3)%26000)
+print(((result**3)%26000)/5)
+print(type(str(((result**3)%26000)/5)))
+
+
+
+
+
+
+
+
+
+
