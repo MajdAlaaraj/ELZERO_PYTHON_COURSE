ecf6f7be3314d4f2115c37fce95c5ae87cd1f76f
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-05 20:12:57 +0300

    python projects

---
+++ Assignments 026-032.py
@@ -0,0 +1,18 @@
+#Assignment 1
+my_List=[1,5,4,3,3,2,1]
+#Sunique_list=
+
+
+#Assignment 2
+nums={1,2,3}
+letters={"A","B","C"}
+print(nums|letters)
+print(nums.union(letters))
+print(nums.symmetric_difference(letters))
+
+#Assignment 3
+my_set={1,2,3}
+letters={"A","B","C"}
+
+
+
