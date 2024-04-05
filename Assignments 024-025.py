50c8e656f6fb6e6bc361fe522e842f27b6561bb0
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-05 18:00:10 +0300

    python projects

---
+++ Assignments 024-025.py
@@ -0,0 +1,17 @@
+#Assignment 1
+myName="Osama",
+print(myName)
+print(type(myName))
+#Assignment 2
+friends=("Osama","Ahmad","Sayed")
+friend="Elzero",
+newFriends=friend+friends[1:3]
+print(newFriends)
+print(type(friends))
+print("{:d} Elements".format(len(friends)))
+#Assignment 3
+nums=(1,2,3)
+letters=("A","B","C")
+nums_and_letters=nums+letters
+print("nums_and_letters_one = {}".format(nums_and_letters))
+print("{} Elements".format(len(nums_and_letters)))
