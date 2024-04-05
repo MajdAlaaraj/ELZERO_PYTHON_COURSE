e7369299578304cc051f5d6f3d921c6d4d6083b0
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-05 21:44:04 +0300

    python projects

--- Assignments 026-032.py
+++ Assignments 026-032.py
@@ -13,6 +13,19 @@ print(nums.symmetric_difference(letters))
 #Assignment 3
 my_set={1,2,3}
 letters={"A","B","C"}
+print(my_set)
+my_set.clear()
+print(my_set)
+my_set.add("B")
+my_set.add("A")
+print(my_set)
+print(my_set.discard("C"))
+#Assignment 4
+set_one={1,2,3}
+set_two={1,2,3,4,5,6}
+print(set_one.issubset(set_two))
+#Assignment 5
 
 
+}
 
