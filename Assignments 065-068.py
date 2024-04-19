b3e8a6643194a1c3d603febe076402c8e805864c
MajdAlaaraj <105524284+MajdAlaaraj@users.noreply.github.com>
2024-04-19 17:00:10 +0300

    Read Me File

--- Assignments 065-068.py
+++ Assignments 065-068.py
@@ -20,5 +20,29 @@ for i in range(50) :
 file.close
 
 #Assignment 3 
-file = open(rf"C:\Users\Dell\Desktop\Python\txt1.txt","a")
+file = open(rf"C:\Users\Dell\Desktop\Python\txt1.txt","+rt")
+num = file.readlines()
+file.close()
+file = open(rf"C:\Users\Dell\Desktop\Python\txt1.txt","+rt")
+num2 = file.read()
+count = 0
+ran = len(num2)
+for i in range(0,ran) :
+    if num2[i] == " " :
+        count+=1 
+
+file.write(f"Numbers Of Lines is => {len(num)}\n")
+file.write(f"Numbers Of Words is => {count}\n")
+file.write(f"Numbers Of Characters is >= {len(num2)}\n")
+file.close()
+file = open(rf"C:\Users\Dell\Desktop\Python\txt1.txt","+rt")
+num3 = file.read()
+count2 =0
+for i in range(0,ran) :
+    if num3[i]=="l" or num3[i] == "L" :
+        count2+=1
+
+file.write(f"Numbers Of Words is => {count2}\n")
+
+
 
